783819d5c9b1645e95ff3edf83ac22aa15043a21
Ken Ho <kenmho38@hotmail.com>
2020-01-20 19:47:17 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -34,25 +34,9 @@ end
 # Your code after this point
 
 def movies_with_director_key(name, movies_collection)
-  # GOAL: For each Hash in an Array (movies_collection), provide a collection
-  # of movies and a directors name to the movie_with_director_name method
-  # and accumulate the returned Array of movies into a new Array that's
-  # returned by this method.
-  #
-  # INPUT:
-  # * name: A director's name
-  # * movies_collection: An Array of Hashes where each Hash represents a movie
-  #
-  # RETURN:
-  #
-  # Array of Hashes where each Hash represents a movie; however, they should all have a
-  # :director_name key. This addition can be done by using the provided
-  # movie_with_director_name method
-
   result = []
   i = 0
-
-  while i < movies_collection.length do
+while i < movies_collection.length do
     movie_data = movies_collection[i]
     result << movie_with_director_name(name, movie_data)
     i += 1
@@ -63,15 +47,7 @@ end
 
 
 def gross_per_studio(collection)
-  # GOAL: Transform NDS'
-  #
-  # INPUT:
-  # * collection: Array of Hashes where each Hash where each Hash represents a movie
-  #
-  # RETURN:
-  #
-  # * Hash whose keys are the studio names and whose values are the sum
-  #   total of all the worldwide_gross numbers for every movie in the input Hash
+
   result = {}
   i = 0
 
@@ -90,17 +66,7 @@ def gross_per_studio(collection)
 end
 
 def movies_with_directors_set(source)
-  # GOAL: For each director, find their :movies Array and stick it in a new Array
-  #
-  # INPUT:
-  # * source: An Array of Hashes containing director information including
-  # :name and :movies
-  #
-  # RETURN:
-  #
-  # Array of Arrays containing all of a director's movies. Each movie will need
-  # to have a :director_name key added to it. You should use the provided
-  # do that work in the movies_with_director_key method
+
 
   i = 0
   a_o_a_movies_by_dir = []
@@ -116,9 +82,7 @@ def movies_with_directors_set(source)
   a_o_a_movies_by_dir
 end
 
-# ----------------    End of Your Code Region --------------------
-# Don't edit the following code! Make the methods above work with this method
-# call code. You'll have to "see-saw" to get this to work!
+
 
 def studios_totals(nds)
   a_o_a_movies_with_director_names = movies_with_directors_set(nds)
